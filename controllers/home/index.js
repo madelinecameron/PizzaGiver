f23027a77e1a62543be318fc0690967aba52e749
Madeline Cameron <madeline@madelinecameron.net>
2015-09-07 11:58:42 -0500

    Fixing text

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Tesssst' });
+  res.render('home', { title: 'give.pizza' });
 });
 
 module.exports = router;
